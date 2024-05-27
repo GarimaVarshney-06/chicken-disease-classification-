0d37f6751b00ed3ae4d32e3743ff8b6e1055f006
Garima Varshney <gungunvarshney00@gmail.com>
2024-05-27 16:04:03 +0530

    prepare_base_model

--- template.py
+++ template.py
@@ -19,8 +19,9 @@ list_of_files = [
     f"src/{project_name}/pipeline/__init__.py",
     f"src/{project_name}/entity/__init__.py",
     f"src/{project_name}/constants/__init__.py",
-    "config/config.yaml",
+    
     "dvc.yaml",
+    "config/config.yaml",
     "params.yaml",
     "requirements.txt",
     "setup.py",
