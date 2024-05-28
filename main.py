ab6ea77afca423a0987de5ed219a58383254301f
Garima Varshney <gungunvarshney00@gmail.com>
2024-05-28 13:18:55 +0530

    training

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from CNNclassifier import logger
 from CNNclassifier.pipeline.stage01_data_ingestion import DataIngestionTrainingPipeline
 from CNNclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
-
+from CNNclassifier.pipeline.stage03_training import ModelTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -26,4 +26,14 @@ except Exception as e:
 
 
 
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = ModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
